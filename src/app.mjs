e42c7728823b8b3798fe123f215f25f6f58e61ea
Justin Dennison <justin1dennison@gmail.com>
2021-09-22 13:21:31 -0400

    [wip]: routing in progress

--- src/app.mjs
+++ src/app.mjs
@@ -1,11 +1,49 @@
 import http from 'http'
+import { buffer } from 'stream/consumers'
+import Router from './router.mjs'
 
-const defaults = {}
-export default function build({ isDev = true,  } = defaults) {
-  
+export default function build({ isDev = true } = {}) {
+  const router = new Router()
 
-  return http.createServer((req, res) => {
-     res.end('Hello')
+  router.put('/', (request, response) => {
+    response.end('works')
   })
-  
+
+  router.del('/', (request, response) => {
+    console.log({ request })
+  })
+
+  router.get('/inventory', (request, response) => {
+    console.log({ request })
+    response.end('inventory')
+  })
+
+  router.get(
+    '/inventory/:id',
+    withMiddleware([])((request, response) => {
+      console.log({ request })
+      response.end(request.url)
+    })
+  )
+
+  return http.createServer(async (request, response) => {
+    await _body(request)
+    await _querystring(request)
+    router.handle(request, response)
+  })
+}
+
+async function _body(request) {
+  const buffers = []
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+  request.body = JSON.parse(Buffer.concat(buffers).toString())
+}
+async function _querystring(request) {
+  const { searchParams } = new URL(
+    request.url,
+    `http://${request.headers.host}`
+  )
+  request.query = { ...searchParams }
 }
--- src/router.mjs
+++ src/router.mjs
@@ -1,32 +1,75 @@
-import { parse } from 'url'
+import { match } from 'path-to-regexp'
+import { URL } from 'url'
 
-export default function Router() {
-  if (!(this instanceof Router)) {
-    return new Router()
+function defaultNotFound(request, response) {
+  response.end('Not Found')
+}
+function defaultErrorHandler(error, request, response) {
+  response.end('Error: ' + error.message)
+}
+
+export default class Router {
+  constructor({
+    notFoundHandler = defaultNotFound,
+    errorHandler = defaultErrorHandler,
+  } = {}) {
+    this.routes = []
+    this.notFoundHandler = notFoundHandler
+    this.errorHandler = errorHandler
   }
 
-  this.routes = []
+  get(path, handler) {
+    this.routes.push(Route.of('GET', path, handler))
+  }
+  post(path, handler) {
+    this.routes.push(Route.of('POST', path, handler))
+  }
+  put(path, handler) {
+    this.routes.push(Route.of('PUT', path, handler))
+  }
+  del(path, handler) {
+    this.routes.push(Route.of('DELETE', path, handler))
+  }
+  patch(path, handler) {
+    this.routes.push(Route.of('PATCH', path, handler))
+  }
+  handle(request, response) {
+    const route = this.routes.find((route) => route.match(request))
+    if (!route) return this.notFoundHandler(request, response)
+    try {
+      return route.handle(request, response)
+    } catch (error) {
+      this.errorHandler(error, request, response)
+    }
+  }
 }
 
-Router.prototype.get = function get(path, handler) {
-  this.routes.push(Route('GET', path, handler))
-}
+class Route {
+  constructor(method, path, handler) {
+    this.method = method
+    this.path = path
+    this.handler = handler
+    this.matcher = match(this.path, { decode: decodeURIComponent })
+  }
 
-Router.prototype.handler = function handler(request, response) {
-  const route = this.routes.find((route) => route.shouldHandle(request))
-}
-
-function Route(method, path, handler) {
-  if (!(this instanceof Route)) {
+  static of(method, path, handler) {
     return new Route(method, path, handler)
   }
 
-  this.method = method
-  this.path = path
-  this.handler = handler
-}
+  match(request) {
+    if (request.method !== this.method) return false
 
-Route.prototype.shouldHandle = function shouldHandle(request) {
-  const requrl = URL
-  return request.url === this.path
+    const { pathname } = new URL(request.url, `http://${request.headers.host}`)
+    return this.matcher(pathname)
+  }
+
+  async handle(request, response) {
+    await this._params(request)
+    return this.handler(request, response)
+  }
+
+  async _params(request) {
+    const { pathname } = new URL(request.url, `http://${request.headers.host}`)
+    request.params = { ...this.matcher(pathname).params }
+  }
 }
